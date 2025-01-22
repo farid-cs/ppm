a395026148b0eea4b40659d1195c5f88d1bcb529
Farid <faridfaracli9@gmail.com>
2025-01-22 16:02:54 +0400

    rename Color type to RGB24

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
+#include <stdint.h>
 #include <stdio.h>
 
 typedef struct {
-	unsigned char red;
-	unsigned char green;
-	unsigned char blue;
-} Color;
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+} RGB24;
 
 static const char IMAGE_PATH[] = "image.ppm";
 static const int IMAGE_WIDTH = 800;
 static const int IMAGE_HEIGHT = 800;
-static const Color BLACK = { .red = 0, .green = 0, .blue = 0 };
+static const RGB24 BLACK = { .red = 0, .green = 0, .blue = 0 };
 
 int
 main(void)
 {
-	Color image[IMAGE_HEIGHT*IMAGE_WIDTH];
+	RGB24 image[IMAGE_HEIGHT*IMAGE_WIDTH];
 	FILE *file;
 
 	for (int j = 0; j != IMAGE_HEIGHT; j++)
@@ -24,7 +25,7 @@ main(void)
 	file = fopen(IMAGE_PATH, "wb");
 
 	fprintf(file, "P6\n%d %d\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
-	fwrite(image, sizeof(Color), IMAGE_HEIGHT*IMAGE_WIDTH, file);
+	fwrite(image, sizeof(RGB24), IMAGE_HEIGHT*IMAGE_WIDTH, file);
 
 	fclose(file);
 
